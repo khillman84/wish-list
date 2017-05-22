0214d20cc7fa0cdc2adde74fc53e8a4cd5c3ae07
Kyle Hillman <khillman84@gmail.com>
2017-05-22 13:48:39 -0700

    added data models and basic UI set up (#1)

---
+++ wish-list/Store+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Store+CoreDataClass.swift
+//  wish-list
+//
+//  Created by Kyle Hillman on 5/22/17.
+//  Copyright © 2017 Kyle Hillman. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Store)
+public class Store: NSManagedObject {
+
+}
