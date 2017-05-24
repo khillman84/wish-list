b3862d72951c1bc497177724e81695b790dd6b1b
Kyle Hillman <khillman84@gmail.com>
2017-05-24 10:44:07 -0700

    Cell setup (#2)
    * added outlets to view controllers and set up fetch methods to core data
    
    * saving and fetching from core data

---
+++ wish-list/ItemCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemCell.swift
+//  wish-list
+//
+//  Created by Kyle Hillman on 5/24/17.
+//  Copyright © 2017 Kyle Hillman. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var pic: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+
+    func configureCell(item: Item) {
+        title.text = item.title
+        price.text = "$\(item.price)"
+        details.text = item.details
+    }
+}
