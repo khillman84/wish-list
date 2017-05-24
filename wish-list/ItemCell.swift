5faa4e46347af785cd9ac70fddf1d41586d44a59
Kyle Hillman <khillman84@gmail.com>
2017-05-24 10:20:46 -0700

    added outlets to view controllers and set up fetch methods to core data

---
+++ wish-list/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  wish-list
+//
+//  Created by Kyle Hillman on 5/24/17.
+//  Copyright © 2017 Kyle Hillman. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var pic: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+
+
+}
